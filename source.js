64109b046d82abc78ff9ef304c16411a1afadbd0
NateD24 <nathadelapena24@g.crookcountyschools.org>
2021-11-19 10:07:13 -0800

    work on stuff

--- source.js
+++ source.js
@@ -9,8 +9,7 @@ if (numb === "1") {
 else if (numb === "2"){
    alert('You chose to ignore the spoon and continue on your journey. After exploring the whole hut you choose to walk outside. As soon as you walk outside a super spicky spider lunges out from the depths. Without something to hit it away with, it sticks to you and bites you. You get extremly sick from the poison and die a slow painful death');
    document.write('<h1>You got ending 1 of 10</h1>')
-}
+
 }
 
-
-// let numb = prompt ('After escaping to saftey you look around and realize you are more lost then you already were. After exploring for a little bit you find a rather larger trail that seems to follow somewhere, but you dont know where. You have two options you can either keep looking around and hope to find something new or you could hop on the trail and follow that 1 for keep looking around 2 for hop on the trail')
+ prompt ('After escaping to saftey you look around and realize you are more lost then you already were. After exploring for a little bit you find a rather larger trail that seems to follow somewhere, but you dont know where. You have two options you can either keep looking around and hope to find something new or you could hop on the trail and follow that 1 for keep looking around 2 for hop on the trail')
